5e2272a5b88286dd9254e7cf35fe0ac70373524e
amdat99 <aamirdat99@gmail.com>
2021-01-12 14:52:04 +0000

    styling components

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+
+
+
+import './homepage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
